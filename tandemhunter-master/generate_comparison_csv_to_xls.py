99675b125eb0bf0de09ab5a99fb8620b9f315168
Nadia Mohammed <nadia.mohammed@addenbrookes.nhs.uk>
2022-08-05 09:21:58 +0100

    changed for loop, added indexing to merge tuples and make merged dataframe

--- tandemhunter-master/generate_comparison_csv_to_xls.py
+++ tandemhunter-master/generate_comparison_csv_to_xls.py
@@ -40,26 +40,42 @@ def generate_PTD_KMT2A_df(args):
     -PTD_KMT2A(df):df of normalized coverages for KMT2A exons 3 and 27, fold coverage difference
     between exons and algorithm predictions for PTDs in KMT2A (as either true/false)
     """
-    # read in intervals from first per targets coverage files
-    # and add to coverage_ratio_df
-    PTD_KM2TA_df = pd.read_csv(
-        args.comparison_csv[0], delimiter=',', header=0
-    )
-    print(PTD_KM2TA_df)
-    #print(type(PTD_KM2TA_df))
-    # read all files in and add to coverage_ratio_df but ignore header row
-    #add this to PTD_KM2TA_df
-    #need to exclude first file though
-    for file in args.comparison_csv:
-        KMT2A_dfs =pd.read_csv()
-        PTD_KM2TA_1 = pd.concat[[KMT2A_dfs, PTD_KM2TA_df]]
-        print(PTD_KM2TA_1)
-            #lines = fh.readlines()[1:]#.Skip(1).FirstOrDefault()
-            #PTD_KM2TA_df = append(lines, )
-            #print(type(lines))
-            #print(lines)
-    #print(PTD_KM2TA_df)
+    #create empty list for dataframes
+    all_data = []
+    for index, file in enumerate(args.comparison_csv):
+        df = pd.read_csv(file.name, delimiter=',')
+    #for index,file in enumerate(args.comparison_csv):
+    #    if index == 0:
+    #        df = pd.read_csv(file.name, delimiter=',')
+    #    else:
+    #        df = pd.read_csv(file.name, delimiter=',') # , header=None, skiprows=[0])
+        #df = pd.concat(map(pd.read_csv, file.name), ignore_index=True)
+        #print(index)
+        print(df)
+        all_data.append(df)
+    print(all_data)
+    merged_data = pd.concat(all_data)
+    print(merged_data)
+    exit
+    #print (KMT2A_dfs)
 
+    #KMT2A_dfs = pd.concat(map(KMT2A_dfs, file))
+    #print(type(KMT2A_dfs))
+    #need to merge dataframes
+    #print(type(KMT2A_dfs))
+    #PTD_KM2TA_1 = pd.concat([PTD_KM2TA_df,KMT2A_dfs])
+    #print(PTD_KM2TA_1)
+    #PTD_KM2TA_1 = PTD_KM2TA_df.append([KMT2A_dfs, PTD_KM2TA_df],ignore_index= True)
+    #print(PTD_KM2TA_1)
+    #PTD_KM2TA_1 = PTD_KM2TA_1.drop_duplicates(keep=False)
+    #print(PTD_KM2TA_1)
+    # check there are no duplicates in PTD_KM2TA_1
+    #print(PTD_KM2TA_1)
+    #lines = fh.readlines()[1:]#.Skip(1).FirstOrDefault()
+    #PTD_KM2TA_df = append(lines, )
+    #print(type(lines))
+    #print(lines)
+    #print(PTD_KM2TA_df)
 
     #put tandemhunter output into df
     #file_df = pd.read_csv(
@@ -72,7 +88,7 @@ def generate_PTD_KMT2A_df(args):
     #check to ensure no duplicates
     #assert
 
-    return PTD_KM2TA_1
+    return merged_data
 
 def KM2TA_df_to_xls(PTD_KM2TA_1):
    """"Generates .xls spreadsheet with PTD predictions from TandemHunter
