63248763931dea6e12651bd8c719ce4686697dcb
Nadia Mohammed <nadia.mohammed@addenbrookes.nhs.uk>
2022-08-17 13:43:14 +0100

    formatting spaces

--- Picard_inputs/KMT2A_exon_processing.R
+++ Picard_inputs/KMT2A_exon_processing.R
@@ -6,8 +6,8 @@ KMT2A_coord <- read_csv("ExonsSpreadsheet-Homo_sapiens_Transcript_Exons_ENST0000
 colnames(KMT2A_coord)[2] <- "Exon_Intron"
 KMT2A_exons <- drop_na(KMT2A_coord)
 #Make a new dataframe with chr, start and end coordinate
-KMT2A_bed<- select(KMT2A_exons, c(3,4))
-KMT2A_bed<- cbind(chr= "chr11", KMT2A_bed)
+KMT2A_bed <- select(KMT2A_exons, c(3,4))
+KMT2A_bed <- cbind(chr= "chr11", KMT2A_bed)
 #Write dataframe into bed file 
 write.table(KMT2A_bed, file = "KMT2A_exons.bed", sep = "\t",
             row.names = FALSE, col.names = FALSE)
